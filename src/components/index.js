98568a7e769a9f5e731d9e2a24c59aadaea0f7d9
vaha77 <abdulvohid996@gmail.com>
2022-05-10 09:04:55 +0900

    ks

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar/index";
+export { default as Header } from "./Header/index";
+export { default as Main } from "./Main/index";
+export { default as Footer } from "./Footer/index";
---
+++ src/root/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer, Header, Main, Navbar } from "../components";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Root;
